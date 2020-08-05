46e894eff970c248cbf9a10cd44bdc974afc16d9
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-05 00:12:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,10 @@ class Dog
     save
   end
   
+   def save 
+    @@all << self 
+  end
+  
   def self.all
     @@all
   end
@@ -20,9 +24,6 @@ class Dog
     @@all.each do |name|
       puts name.name
     
-  end
-end
-  def save 
-    @@all << self 
+    end
   end
 end
